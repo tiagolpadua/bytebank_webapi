4d451bfab130d7271a585930003921a435866d41
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2020-08-01 20:35:15 -0300

    b01a04

---
+++ lib/components/transaction_auth_dialog.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TransactionAuthDialog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('Authenticate'),
+      content: TextField(
+        obscureText: true,
+        maxLength: 4,
+        decoration: InputDecoration(
+          border: OutlineInputBorder()
+        ),
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 64, letterSpacing: 32),
+        keyboardType: TextInputType.number,
+      ),
+      actions: <Widget>[
+        FlatButton(
+          onPressed: () {
+            print('cancel');
+          },
+          child: Text('Cancel'),
+        ),
+        FlatButton(
+          onPressed: () {
+            print('confirm');
+          },
+          child: Text('Confirm'),
+        ),
+      ],
+    );
+  }
+}
