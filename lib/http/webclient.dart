820124d32e15d0b5b89b96c7bad0ab4b4e622073
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2020-08-01 16:18:50 -0300

    a02a06

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,6 +1,37 @@
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_client_with_interceptor.dart';
+import 'package:http_interceptor/interceptor_contract.dart';
+import 'package:http_interceptor/models/request_data.dart';
+import 'package:http_interceptor/models/response_data.dart';
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    print('Request');
+    print('url: ${data.url}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+    print('Response');
+    print('status code: ${data.statusCode}');
+    print('url: ${data.url}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
+}
 
 void findAll() async {
-  final Response response = await get('http://192.168.56.1:8080/transactions');
-  print(response.body);
+  final Client client = HttpClientWithInterceptor.build(
+    interceptors: [
+      LoggingInterceptor(),
+    ],
+  );
+
+  final Response response =
+      await client.get('http://192.168.56.1:8080/transactions');
 }
