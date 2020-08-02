2d7d18f8b04c4523500e03fea9c9690d5af9b952
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2020-08-02 09:37:27 -0300

    c02a05

--- test/dashboard_widget_test.dart
+++ test/dashboard_widget_test.dart
@@ -10,12 +10,43 @@ void main() {
     expect(mainImage, findsOneWidget);
   });
 
-  testWidgets('should display the transfer feature when the dashboard is opened',
+  testWidgets(
+      'should display the transfer feature when the dashboard is opened',
       (tester) async {
     await tester.pumpWidget(MaterialApp(home: Dashboard()));
-    final iconTransferFeatureItem = find.widgetWithIcon(FeatureItem, Icons.monetization_on);
-    expect(iconTransferFeatureItem, findsOneWidget);
-    final nameTransferFeatureItem = find.widgetWithText(FeatureItem, 'Transfer');
-    expect(nameTransferFeatureItem, findsOneWidget);
+//    final iconTransferFeatureItem =
+//        find.widgetWithIcon(FeatureItem, Icons.monetization_on);
+//    expect(iconTransferFeatureItem, findsOneWidget);
+//    final nameTransferFeatureItem =
+//        find.widgetWithText(FeatureItem, 'Transfer');
+//    expect(nameTransferFeatureItem, findsOneWidget);
+
+    final transferFeatureItem = find.byWidgetPredicate((widget) =>
+        featureItemMatcher(widget, 'Transfer', Icons.monetization_on));
+
+    expect(transferFeatureItem, findsOneWidget);
   });
+
+  testWidgets(
+      'should display the transaction feed feature when the dashboard is opened',
+      (tester) async {
+    await tester.pumpWidget(MaterialApp(home: Dashboard()));
+//    final iconTransactionFeedFeatureItem =
+//        find.widgetWithIcon(FeatureItem, Icons.description);
+//    expect(iconTransactionFeedFeatureItem, findsOneWidget);
+//    final nameTransactionFeedFeatureItem =
+//        find.widgetWithText(FeatureItem, 'Transfer');
+
+    final transactionFeedFeatureItem = find.byWidgetPredicate((widget) =>
+        featureItemMatcher(widget, 'Transaction Feed', Icons.description));
+
+    expect(transactionFeedFeatureItem, findsOneWidget);
+  });
+}
+
+bool featureItemMatcher(widget, String name, IconData icon) {
+  if (widget is FeatureItem) {
+    return widget.name == name && widget.icon == icon;
+  }
+  return false;
 }
