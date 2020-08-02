c52c4cb02b71dfd7544545bae4f86c0888104aea
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2020-08-02 00:42:16 -0300

    c01a08

---
+++ test/dashboard_widget_test.dart
@@ -0,0 +1,19 @@
+import 'package:bytebank/screens/dashboard.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('should display the main image when the dashboard is opened',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: Dashboard()));
+    final mainImage = find.byType(Image);
+    expect(mainImage, findsOneWidget);
+  });
+
+  testWidgets('should display the first feature when the dashboard is opened',
+      (tester) async {
+    await tester.pumpWidget(MaterialApp(home: Dashboard()));
+    final firstFeature = find.byType(FeatureItem);
+    expect(firstFeature, findsWidgets);
+  });
+}
