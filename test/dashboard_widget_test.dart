b8ec238d20f5e0e355600d7ba850878f38f2c674
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2020-08-02 09:54:03 -0300

    c03a01

--- test/dashboard_widget_test.dart
+++ test/dashboard_widget_test.dart
@@ -2,6 +2,8 @@ import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'matchers.dart';
+
 void main() {
   testWidgets('should display the main image when the dashboard is opened',
       (WidgetTester tester) async {
@@ -44,9 +46,3 @@ void main() {
   });
 }
 
-bool featureItemMatcher(widget, String name, IconData icon) {
-  if (widget is FeatureItem) {
-    return widget.name == name && widget.icon == icon;
-  }
-  return false;
-}
