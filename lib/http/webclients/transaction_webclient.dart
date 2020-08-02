aae8627e59d45c319af7dbe348beba654ba5ee12
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2020-08-01 23:23:52 -0300

    b04a03

--- lib/http/webclients/transaction_webclient.dart
+++ lib/http/webclients/transaction_webclient.dart
@@ -18,6 +18,9 @@ class TransactionWebClient {
   }
 
   Future<Transaction> save(Transaction transaction, String password) async {
+
+    await Future.delayed(Duration(seconds: 10));
+
     final String transactionJson = jsonEncode(transaction.toJson());
 
     final Response response = await client
@@ -30,12 +33,23 @@ class TransactionWebClient {
       return Transaction.fromJson(jsonDecode(response.body));
     }
 
-    throw HttpException(_statusCodeResponses[response.statusCode]);
+    throw HttpException(_getMessage(response.statusCode));
+  }
+
+  String _getMessage(int statusCode) {
+    print('status code: $statusCode');
+    if(_statusCodeResponses.containsKey(statusCode)) {
+      return _statusCodeResponses[statusCode];
+    } else {
+      return 'Unknown error';
+    }
+
   }
 
   static final Map<int, String> _statusCodeResponses = {
     400: 'there was an error submitting transaction',
-    401: 'authentication failed'
+    401: 'authentication failed',
+    409: 'transaction already exists',
   };
 }
 
--- lib/screens/transaction_form.dart
+++ lib/screens/transaction_form.dart
@@ -7,6 +7,7 @@ import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:http_interceptor/models/http_interceptor_exception.dart';
+import 'package:uuid/uuid.dart';
 
 class TransactionForm extends StatefulWidget {
   final Contact contact;
@@ -20,9 +21,11 @@ class TransactionForm extends StatefulWidget {
 class _TransactionFormState extends State<TransactionForm> {
   final TextEditingController _valueController = TextEditingController();
   final TransactionWebClient _webClient = TransactionWebClient();
+  final String transsactionId = Uuid().v4();
 
   @override
   Widget build(BuildContext context) {
+    print('transaction form id $transsactionId');
     return Scaffold(
       appBar: AppBar(
         title: Text('New transaction'),
@@ -68,7 +71,7 @@ class _TransactionFormState extends State<TransactionForm> {
                       final double value =
                           double.tryParse(_valueController.text);
                       final transactionCreated =
-                          Transaction(value, widget.contact);
+                          Transaction(transsactionId, value, widget.contact);
                       showDialog(
                           context: context,
                           builder: (contextDialog) {
