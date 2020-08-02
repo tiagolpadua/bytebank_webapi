5ea20efae4f731c5f8415b217f86c9d98fbd117e
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2020-08-02 00:22:51 -0300

    c01a06

---
+++ test/transaction_test.dart
@@ -0,0 +1,14 @@
+
+import '../lib/models/transaction.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('should return the value when create a transaction', (){
+    final transaction = Transaction(null, 200, null);
+    expect(transaction.value, 200);
+  });
+
+  test('should show error when create transaction with value less than zero', (){
+    expect(() => Transaction(null, 0, null), throwsAssertionError);
+  });
+}
